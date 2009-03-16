4735580d0f47368603efa6d36d5858016a8de279
Amit Levy <aalevy@gmail.com>
2009-03-16 16:47:43 -0700

    Added apache licsense and fomratted java source

--- src/edu/washington/cs/cse490h/donut/Donut.java
+++ src/edu/washington/cs/cse490h/donut/Donut.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2009 Amit Levy, Jeff Prouty, Rylan Hawkins
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.washington.cs.cse490h.donut;
 
 import com.google.inject.Guice;
@@ -12,6 +28,9 @@ import edu.washington.cs.cse490h.donut.server.RequestModule;
 import edu.washington.cs.cse490h.donut.server.ServerModule;
 import edu.washington.cs.cse490h.donut.business.TNode;
 
+/**
+ * @author alevy
+ */
 public class Donut {
     public static void main(String[] args) throws Exception {
         DonutModule donutModule = new DonutModule();
